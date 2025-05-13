bffa305c6a5bfd6472a934944142e51e6b2d4f8b
Nagashiva Abbu <shivanagashiva05@gmail.com>
2025-05-13 12:20:46 +0530

    Add files via upload

---
+++ src/main/java/com/srms/StudentResultManagementSystem1Application.java
@@ -0,0 +1,13 @@
+package com.srms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultManagementSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentResultManagementSystem1Application.class, args);
+	}
+
+}
