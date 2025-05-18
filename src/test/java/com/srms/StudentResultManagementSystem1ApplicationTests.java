40e2953e92cda12be16439e55b49e83feaad57ee
Nagashiva Abbu <shivanagashiva05@gmail.com>
2025-05-19 01:08:06 +0530

    Add files via upload

---
+++ src/test/java/com/srms/StudentResultManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentResultManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
